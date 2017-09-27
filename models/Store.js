d005f13ea2144767b7ecd651986b3f4abb4eaa8d
Djindja <stefansdjindjic@gmail.com>
2017-09-27 23:36:33 +0200

    small fixes

--- models/Store.js
+++ models/Store.js
@@ -2,28 +2,28 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const slug = require('slugs');
 
-const storeChema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: 'Please enter a store name'
-    },
-    slug: String,
-    description: {
-        type: String,
-        trim: true
-    },
-    tags: [String]
+const storeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name!'
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String]
 });
 
-storeChema.pre('save', function(next){
-    if (!this.isModified('name')) {
-        next(); // skip it
-        return; // stop this function from running
-    }
-    this.slug = slug(this.name);
-    next();
-    // TODO make more resiliant so slugs are unique
+storeSchema.pre('save', function(next) {
+  if (!this.isModified('name')) {
+    next(); // skip it
+    return; // stop this function from running
+  }
+  this.slug = slug(this.name);
+  next();
+  // TODO make more resiliant so slugs are unique
 });
 
-module.exports = mongoose.model('Store', storeChema);
+module.exports = mongoose.model('Store', storeSchema);
